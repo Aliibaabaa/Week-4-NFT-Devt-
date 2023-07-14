d9004a16800297bab1dfdd52e2842e52f685331a
Ali <princealy.nueles@gmail.com>
2023-07-14 15:02:19 +0800

    added July14 notes

---
+++ July14/Notes.sol
@@ -0,0 +1,14 @@
+/**
+    https://docs.opensea.io/
+    https://docs.opensea.io/docs/metadata-standards
+
+==* InterPlanetary File System (IPFS) *== 
+     └―> decentralized file storage system that allows you to store and share files on a peer-to-peer network. 
+     └―> allows us to upload, store and manage files on the blockchain.
+
+==* Metadata *== 
+     └―> holds all the information about your image or NFT.
+
+==* Opensea Metadata *== 
+     └―> a popular NFT marketplace that has its own metadata standard. The OpenSea metadata standard is designed to be easy to use and understand.
+ */
