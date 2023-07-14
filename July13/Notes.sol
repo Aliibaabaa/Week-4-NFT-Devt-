08943bb3e0c3e3f0f667b84051d69e96c54f1739
Ali <princealy.nueles@gmail.com>
2023-07-14 14:41:36 +0800

    added July13 notes

---
+++ July13/Notes.sol
@@ -0,0 +1,19 @@
+/**
+== Additional Resources: ==
+    https://academy.binance.com/en/articles/what-is-an-nft
+    https://ethereum.org/en/nft/
+    https://docs.openzeppelin.com/contracts/
+    https://eips.ethereum.org/EIPS/eip-721
+*/ 
+
+/**
+ ==* NFT *== 
+     └―> unique digital assets stored on a blockchain, representing ownership of various items like art, music, videos, to in-game items, and serve as a digital record of ownership, and more. They cannot be replaced and have a unique identification code and metadata for proof of ownership. 
+
+ ==* ERC721 *== 
+     └―> The ERC-721 standard is an Ethereum Improvement Proposal (EIP) that defines a standard for creating and using non-fungible tokens (NFTs) on the Ethereum blockchain.
+
+ ==* ERC1155 *== 
+     └―> ERC-1155 is a multi-token standard on the Ethereum blockchain that allows for the creation of both fungible and non-fungible tokens (NFTs). 
+ */
+
--- July13/SimpleNFT.sol
+++
@@ -1,8 +0,0 @@
-/**
-== Additional Resources: ==
-    https://academy.binance.com/en/articles/what-is-an-nft
-    https://ethereum.org/en/nft/
-    https://docs.openzeppelin.com/contracts/
-    https://eips.ethereum.org/EIPS/eip-721
-*/ 
-
