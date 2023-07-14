a134d49ce45d6d2dba4f1a982afc59270ae333bf
Ali <princealy.nueles@gmail.com>
2023-07-14 14:16:21 +0800

    added July13 notes

---
+++ July13/SimpleNFT.sol
@@ -0,0 +1,8 @@
+/**
+== Additional Resources: ==
+    https://academy.binance.com/en/articles/what-is-an-nft
+    https://ethereum.org/en/nft/
+    https://docs.openzeppelin.com/contracts/
+    https://eips.ethereum.org/EIPS/eip-721
+*/ 
+
